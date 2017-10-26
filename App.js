2e6bb043e40a302807d9e7ef40604ea3068f5baa
Jan Vlcek <vlki@vlki.cz>
2017-10-26 17:53:03 +0200

    Spaces indentation

--- App.js
+++ App.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { AsyncStorage, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-	componentDidMount() {
-		setInterval(this.tick, 5000)
-	}
+  componentDidMount() {
+    setInterval(this.tick, 5000)
+  }
 
-	tick() {
-		AsyncStorage.setItem('foo', 'a'.repeat(5000000))
-	}
+  tick() {
+    AsyncStorage.setItem('foo', 'a'.repeat(5000000))
+  }
 
   render() {
     return (
       <View style={styles.container}>
-				<Text>This app saves huge string via AsyncStorage every 5 seconds</Text>
+        <Text>This app saves huge string via AsyncStorage every 5 seconds</Text>
       </View>
     );
   }
